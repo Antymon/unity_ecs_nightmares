e1fc2bea60ad66cd9a8eb4710ca507bafa81869b
szymon <Szymon.Brych@gmail.com>
2017-09-26 08:49:38 +0200

    Sketching powerups interfaces

---
+++ Assets/Scripts/Systems/Effects/AddHealthEffect.cs
@@ -0,0 +1,22 @@
+﻿using Entitas;
+
+public class AddHealthEffect : IEffect
+{
+    public int healthPoints;
+
+    public void Apply(GameEntity entity)
+    {
+        entity.health.healthPoints=System.Math.Min(entity.health.healthPoints+healthPoints, entity.health.healthPointsCap);
+    }
+
+    public int GetRepeatInterval()
+    {
+        return 0;
+    }
+
+    public bool IsContintous()
+    {
+        return false;
+    }
+}
+
