e592fe7b26c02b4929671ddd389c9f379c5d1e2d
szymon <Szymon.Brych@gmail.com>
2017-09-28 10:09:49 +0200

    Fixing transform caching

--- Assets/Scripts/Behaviours/Base/TransformResettingBindingBehaviour.cs
+++ Assets/Scripts/Behaviours/Base/TransformResettingBindingBehaviour.cs
@@ -5,11 +5,26 @@ public class TransformResettingBindingBehaviour : BindingEntitasBehaviour
 {
     private Vector3 position;
     private Quaternion rotation;
+    private bool transformCached = false;
+
+    public void Awake()
+    {
+        CacheTransform();
+    }
 
     public override void DeserializeEnitity(GameEntity entity)
     {
+        CacheTransform();
  	    base.DeserializeEnitity(entity);
+    }
 
+    //depending on instantiation - unity or factory method, one or other start point may be called
+    private void CacheTransform()
+    {
+        if(transformCached)
+        {
+            return;
+        }
         position = transform.position;
         rotation = transform.rotation;
     }
